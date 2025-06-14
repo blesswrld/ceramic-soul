d75b653c89155c696f10e8a5a680dbae8cd351a5
blesswrld <roninfortnite71@gmail.com>
2025-06-14 23:04:09 +0300

    add slider component (need to improve)

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,44 @@
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
 // Импорт файлов из purecss
-import "purecss/build/grids-min.css";
-import "purecss/build/grids-responsive-min.css";
+// import "purecss/build/grids-min.css";
+// import "purecss/build/grids-responsive-min.css";
 
 // Импорт общего файла стилей
 import "/src/sass/style.scss";
+
+try {
+    new Swiper(".works__slider", {
+        slidesPerView: 1,
+        loop: true,
+
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+
+        breakpoints: {
+            // when window width is >= 1200px
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 5,
+            },
+            1920: {
+                slidesPerView: 3,
+                spaceBetween: 35,
+            },
+        },
+
+        // configure Swiper to use modules
+        modules: [Navigation, Pagination],
+    });
+} catch (e) {}
