758b9c019d1d0be2ef718683e11a963f4f2c2222
blesswrld <roninfortnite71@gmail.com>
2025-06-19 00:01:38 +0300

    add burger component

--- src/js/script.js
+++ src/js/script.js
@@ -12,6 +12,21 @@ import "purecss/build/grids-responsive-min.css";
 // Импорт общего файла стилей
 import "/src/sass/style.scss";
 
+// Логика бургера
+const burger = document.querySelector(".burger"),
+    close = document.querySelector(".header__menu-close"),
+    menu = document.querySelector(".header__menu");
+
+burger.addEventListener("click", () => {
+    menu.classList.add("header__menu_active");
+    document.body.style.overflow = "hidden";
+});
+
+close.addEventListener("click", () => {
+    menu.classList.remove("header__menu_active");
+    document.body.style.overflow = "";
+});
+
 try {
     new Swiper(".works__slider", {
         slidesPerView: 1,
